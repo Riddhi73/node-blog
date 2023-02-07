253ee247ab079f084b921c51537f804af0ece737
Riddhiman Debnath <riddimandebnath4260@gmail.com>
2023-02-07 14:38:29 +0600

    Pass data into views

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ const express = require("express");
 
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.listen(3000);
 
-app.get("/index", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+app.get("/", (req, res) => {
+  const blogs = [
+    {title:"Mario is dead", snippet:"Lorem ipsum dolor sit amet consectetur"},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ]
+  res.render('index', {title: 'Home', blogs});
 });
 
-app.get("/about-us",(req,res) =>{
-    res.redirect('/about')
+app.get("/about", (req, res) => {
+  res.render('about');
+});
+
+app.get('/blogs/create',(req,res)=>{
+  res.render('create')
 })
 
 app.use((req,res) => {
-    res.sendFile('./views/404.html', {root : __dirname})
+    res.render('404')
 })
