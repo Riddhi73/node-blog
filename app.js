80f90e03f9e77cd331aa09825f0ec46dfeeb91f3
Riddhiman Debnath <riddimandebnath4260@gmail.com>
2023-02-07 19:17:07 +0600

    Use middleware

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 const express = require("express");
-
+const morgan = require('morgan')
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 app.listen(3000);
 
+app.use(morgan('dev'))
+
 app.get("/", (req, res) => {
   const blogs = [
-    {title:"Mario is dead", snippet:"Lorem ipsum dolor sit amet consectetur"},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ]
-  res.render('index', {title: 'Home', blogs});
+    {
+      title: "Mario is dead",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.render('about');
+  res.render("about");
 });
 
-app.get('/blogs/create',(req,res)=>{
-  res.render('create')
-})
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
+});
 
-app.use((req,res) => {
-    res.render('404')
-})
+app.use((req, res) => {
+  res.render("404");
+});
